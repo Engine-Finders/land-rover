aaa0855e626f0c62c9adc926d425b902e7aa66fe
Ahmad Ghazi <enginefinders5@gmail.com>
2026-07-28 23:48:03 +0500

    updated corevariants ocmponent, deployment error fixed, also added sitemap and robots.txt

--- src/components/generation/CoreVariants.js
+++ src/components/generation/CoreVariants.js
@@ -1,3 +1,40 @@
+// export default function CoreVariants({ data }) {
+//   if (!data) return null;
+
+//   return (
+//     <section style={{ paddingBottom: 8 }}>
+//       <h2>Core Variants</h2>
+
+//       {data.dieselVariants?.length > 0 && (
+//         <>
+//           <h3>Diesel</h3>
+//           <ul>
+//             {data.dieselVariants.map((v) => (
+//               <li key={v}>{v}</li>
+//             ))}
+//           </ul>
+//         </>
+//       )}
+
+//       {data.petrolVariants?.length > 0 && (
+//         <>
+//           <h3>Petrol</h3>
+//           <ul>
+//             {data.petrolVariants.map((v) => (
+//               <li key={v}>{v}</li>
+//             ))}
+//           </ul>
+//         </>
+//       )}
+
+//       {data.scopeNote && <p>{data.scopeNote}</p>}
+//       <hr />
+//     </section>
+//   );
+// }
+
+import Link from "next/link";
+
 export default function CoreVariants({ data }) {
   if (!data) return null;
 
@@ -10,7 +47,9 @@ export default function CoreVariants({ data }) {
           <h3>Diesel</h3>
           <ul>
             {data.dieselVariants.map((v) => (
-              <li key={v}>{v}</li>
+              <li key={v.url}>
+                <Link class="" href={v.url}>{v.name}</Link>
+              </li>
             ))}
           </ul>
         </>
@@ -21,7 +60,9 @@ export default function CoreVariants({ data }) {
           <h3>Petrol</h3>
           <ul>
             {data.petrolVariants.map((v) => (
-              <li key={v}>{v}</li>
+              <li key={v.url}>
+                <Link href={v.url}>{v.name}</Link>
+              </li>
             ))}
           </ul>
         </>
